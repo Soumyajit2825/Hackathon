218ea9973b321b2558f1ce3eaf0e9c1bd2df4eb3
Soumyajit <mondalsoumya02@gmail.com>
2022-10-28 15:01:07 +0530

    Files added

--- web.js
+++ web.js
@@ -1,69 +1,130 @@
+
 const msg = document.querySelector('.msg');
-const guess= document.querySelector('input');
-const btn= document.querySelector('.btn');
-const hint=document.querySelector('.hint');
-const show=document.querySelector('.show');
-const skip=document.querySelector('.skip');
 
-let play=false;
-let newWords= "";
-let randWords="";
-let sWords = ['2008','mjolnir','vibranium','tchalla','chitauri','storkclub','gamora','harley',
-'hydra','korg','malekith','morag'];
+const guess = document.querySelector('input');
 
-const createNewWords = function(){
+const btn = document.querySelector('.btn');
+
+const hint = document.querySelector('.hint');
+
+const show = document.querySelector('.show');
+
+const skip = document.querySelector('.skip');
+
+
+
+let play = false;
+
+let newWords = "";
+
+let randWords = "";
+
+let sWords = ['2008', 'Mjolnir', 'Vibranium', 'TChalla', 'Chitauri', 'storkclub', 'gamora', 'harley',
+
+    'hydra', 'cork', 'malekith', 'morag'];
+
+
+
+const createNewWords = function () {
+
     let randNum = Math.floor(Math.random() * sWords.length);
+
     // console.log(randNum);
+
     let newTempSwords = sWords[randNum];
-     return newTempSwords;
+
+    return newTempSwords;
+
 }
 
-const scrambleWords = function(arr){
-    for(let i = arr.length-1; i>0; i--){                            
+
+
+const scrambleWords = function (arr) {
+
+    for (let i = arr.length - 1; i > 0; i--) {
+
         let temp = arr[i];
-        console.log(temp);                  
-        let j = Math.floor(Math.random()*(i+1));                
+
+        console.log(temp);
+
+        let j = Math.floor(Math.random() * (i + 1));
+
         console.log(i);
+
         console.log(j);
 
+
+
         arr[i] = arr[j];
+
         arr[j] = temp;
 
+
+
     }
+
     return arr;
+
 }
 
-skip.addEventListener('click', ()=>{
-
-        // btn.innerHTML="Guess";
-                                        // skip.style.display="block";
-        // guess.classList.toggle('text');
-
-        newWords = createNewWords();
-        randWords = scrambleWords(newWords.split("")).join("");
-        // console.log(randWords.join(""));
-
-        msg.innerHTML = `Unscramble The Answer:- ${randWords}`;
 
 
-        hintQuestions();
+skip.addEventListener('click', () => {
+
+
+
+    // btn.innerHTML="Guess";
+
+    // skip.style.display="block";
+
+    // guess.classList.toggle('text');
+
+
+
+    newWords = createNewWords();
+
+    randWords = scrambleWords(newWords.split("")).join("");
+
+    // console.log(randWords.join(""));
+
+
+
+    msg.innerHTML = `Unscramble The Answer:- ${randWords}`;
+
+
+
+    hintQuestions();
+
 
 
 })
 
 
-btn.addEventListener("click",function(){
-    if(!play){
-        play=true;
-        btn.innerHTML="Guess";
-                                        skip.style.display="block";
+
+btn.addEventListener("click", function () {
+
+    if (!play) {
+
+        play = true;
+
+        btn.innerHTML = "Guess";
+
+        skip.style.display = "block";
+
         guess.classList.toggle('text');
 
+
+
         newWords = createNewWords();
+
         randWords = scrambleWords(newWords.split("")).join("");
+
         // console.log(randWords.join(""));
 
+
+
         msg.innerHTML = `Unscramble The Answer:- ${randWords}`;
+
 
 
         hintQuestions();
@@ -75,241 +136,473 @@ btn.addEventListener("click",function(){
 
 
 
-    } else{
-        let tempWord = guess.value;
-        if(tempWord===newWords){
-            // console.log("sahi");
-            play=false;
-            msg.innerHTML = `CORRECT!, IT IS ${newWords}`;
-            btn.innerHTML="Next";
-            guess.classList.toggle('text');
-            guess.value="";
-            show.style.display="none";
-            skip.style.display="none";
-        }else{
-            // console.log("galat");
-            msg.innerHTML=`ITS INCORRECT. TRY AGAIN -- ${randWords}`;
-            show.style.display="block";
-            show.innerHTML='STUCK ? SHOW ANSWER';
 
-            show.addEventListener("click", ()=>{
+
+
+
+
+
+
+    } else {
+
+        let tempWord = guess.value;
+
+        if (tempWord === newWords) {
+
+            // console.log("sahi");
+
+            play = false;
+
+            msg.innerHTML = `CORRECT!, IT IS ${newWords}`;
+
+            btn.innerHTML = "Next";
+
+            guess.classList.toggle('text');
+
+            guess.value = "";
+
+            show.style.display = "none";
+
+            skip.style.display = "none";
+
+        } else {
+
+            // console.log("galat");
+
+            msg.innerHTML = `ITS INCORRECT. TRY AGAIN -- ${randWords}`;
+
+            show.style.display = "block";
+
+            show.innerHTML = 'STUCK ? SHOW ANSWER';
+
+
+
+            show.addEventListener("click", () => {
+
+
 
                 // let a = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? ');
 
-                if(newWords===sWords[0]){
+
+
+                if (newWords === sWords[0]) {
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let a = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? ');
 
-                    show.innerHTML='2008';
-                    guess.value="2008";
+
+
+                    show.innerHTML = '2008';
+
+                    guess.value = "2008";
+
+
 
                 }
-                else if(newWords===sWords[1]){
+
+                else if (newWords === sWords[1]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='dumbledore';
 
+
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let b = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? ');
 
-                    show.innerHTML='mjolnir';
-                    guess.value="mjolnir";
+
+
+                    show.innerHTML = 'Mjolnir';
+
+                    guess.value = "Mjolnir";
+
+
 
                 }
 
-                else if(newWords===sWords[2]){
+
+
+                else if (newWords === sWords[2]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='tomriddle';
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let c = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER?');
 
-                    show.innerHTML='vibranium';
-                    guess.value="vibranium";
+
+
+                    show.innerHTML = 'Vibranium';
+
+                    guess.value = "Vibranium";
+
+
 
                 }
 
-                else if(newWords===sWords[3]){
+
+
+                else if (newWords === sWords[3]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='hagrid';
 
+
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let d = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER?');
 
-                    show.innerHTML='tchalla';
-                    guess.value="tchalla";
+
+
+                    show.innerHTML = 'TChalla';
+
+                    guess.value = "TChalla";
+
+
 
                 }
 
-                else if(newWords===sWords[4]){
+
+
+                else if (newWords === sWords[4]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='slytherin';
 
+
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let e = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER?');
 
-                    show.innerHTML='chitauri';
-                    guess.value="chitauri";
+
+
+                    show.innerHTML = 'Chitauri';
+
+                    guess.value = "Chitauri";
+
+
 
                 }
 
-                else if(newWords===sWords[5]){
+
+
+                else if (newWords === sWords[5]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='voldemort';
 
+
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let f = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER?');
 
-                    show.innerHTML='storkclub';
-                    guess.value="storkclub";
+
+
+                    show.innerHTML = 'storkclub';
+
+                    guess.value = "storkclub";
+
+
 
                 }
 
-                else if(newWords===sWords[6]){
+
+
+                else if (newWords === sWords[6]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='siriusblack';
 
+
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let g = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER?');
 
-                    show.innerHTML='gamora';
-                    guess.value="gamora";
+
+
+                    show.innerHTML = 'gamora';
+
+                    guess.value = "gamora";
+
+
 
                 }
 
-                else if(newWords===sWords[7]){
+
+
+                else if (newWords === sWords[7]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='spiders';
 
+
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let h = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER?');
-                    if(a===true){
-                    show.innerHTML='harley';
-                    guess.value="harley";
+
+                    if (a === true) {
+
+                        show.innerHTML = 'harley';
+
+                        guess.value = "harley";
+
                     }
+
                 }
 
-                else if(newWords===sWords[8]){
+
+
+                else if (newWords === sWords[8]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='fang';
 
+
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let i = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? ');
 
-                    show.innerHTML='hydra';
-                    guess.value="hydra";
+
+
+                    show.innerHTML = 'hydra';
+
+                    guess.value = "hydra";
+
+
 
                 }
 
-                else if(newWords===sWords[9]){
+
+
+                else if (newWords === sWords[9]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='ginny';
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let j = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? ');
 
-                    show.innerHTML='korg';
-                    guess.value="korg";
+
+
+                    show.innerHTML = 'cork';
+
+                    guess.value = "cork";
+
+
 
                 }
 
-                else if(newWords===sWords[10]){
+
+
+                else if (newWords === sWords[10]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='keeper';
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let k = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER?');
 
-                    show.innerHTML='malekith';
-                    guess.value="malekith";
+
+
+                    show.innerHTML = 'malekith';
+
+                    guess.value = "malekith";
+
+
 
                 }
 
-                else if(newWords===sWords[11]){
+
+
+                else if (newWords === sWords[11]) {
+
                     // confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? IF YES, CLICK AGAIN');
+
                     // show.innerHTML='ginny';
+
                     // show.innerHTML='STUCK ? SHOW ANSWER';
+
                     // let j = confirm('ARE YOU SURE YOU WANT TO SEE THE ANSWER? ');
 
-                    show.innerHTML='morag';
-                    guess.value="morag";
+
+
+                    show.innerHTML = 'morag';
+
+                    guess.value = "morag";
+
+
 
                 }
+
             })
+
         }
+
     }
+
 })
 
 
 
-function hintQuestions(){
-    if(newWords===sWords[0]){
-            hint.innerHTML='HINT:Who has been stealing Harry’s letters from Ron and Hermione at the beginning of ‘Harry Potter and the Chamber of Secrets’? ';
-            console.log("2008");
-
-        }
-        else if(newWords===sWords[1]){
-            hint.innerHTML='HINT: Who has given Harry Potter the Invisibility cloak?';
-            console.log("mjolnir");
-        }
-
-        else if(newWords===sWords[2]){
-            hint.innerHTML='HINT: The actual name of Lord Voldemort is ';
-            console.log("vibranium");
-        }
 
 
-        else if(newWords===sWords[3]){
-            hint.innerHTML='HINT: Who brought Harry to Hogwarts when he was matured to go there?';
-            console.log("tchalla");
-        }
+function hintQuestions() {
 
+    if (newWords === sWords[0]) {
 
-        else if(newWords===sWords[4]){
-            hint.innerHTML='HINT:In his first quidditch match, Harry’s team was against which House';
-            console.log("chitauri");
-        }
+        hint.innerHTML = 'HINT: What year was the first Iron Man movie released, kicking off the Marvel Cinematic Universe?';
+
+        console.log("2008");
 
 
 
+    }
 
+    else if (newWords === sWords[1]) {
 
-        else if(newWords===sWords[5]){
-            hint.innerHTML='HINT: Man behind the scar of harry potter';
-            console.log("storkclub");
-        }
+        hint.innerHTML = 'HINT: What is the name of Thor’s hammer?';
 
-        else if(newWords===sWords[6]){
-            hint.innerHTML="HINT: Who is Harry Potter's godfather?";
-            console.log("gamora");
-        }
+        console.log("Mjolnir");
 
-        else if(newWords===sWords[7]){
-            hint.innerHTML='HINT: Which type of insect is Ronald Weasley most afraid of?';
-            console.log("harley");
-        }
-
-
-        else if(newWords===sWords[8]){
-            hint.innerHTML='HINT: What is the name of Hagrid’s big dog?';
-            console.log("hydra");
-        }
+    }
 
 
 
-        else if(newWords===sWords[9]){
-            hint.innerHTML="HINT: What is the first name of Harry Potter's wife?";
-            console.log("korg");
-        }
+    else if (newWords === sWords[2]) {
+
+        hint.innerHTML = 'HINT: What is Captain America’s shield made of? ';
+
+        console.log("Vibranium");
+
+    }
+
+
+
+    else if (newWords === sWords[3]) {
+
+        hint.innerHTML = 'HINT:  What is the real name of the Black Panther?';
+
+        console.log("Tchalla");
+
+    }
+
+
+
+    else if (newWords === sWords[4]) {
+
+        hint.innerHTML = 'HINT: What is the alien race Loki sends to invade Earth in The Avengers?';
+
+        console.log("Chitauri");
+
+    }
 
 
 
 
 
-        else if(newWords===sWords[10]){
-            hint.innerHTML='HINT:What do you call the person who defends the goal post in Quidditch?';
-            console.log("malekith");
-        }
 
-        else if(newWords===sWords[11]){
-            hint.innerHTML='HINT:What part of the United Kingdom is Hogwarts School located in?';
-            console.log("morag");
-        }
+
+
+
+    else if (newWords === sWords[5]) {
+
+        hint.innerHTML = 'HINT: Where does Peggy tell Steve she wants to meet him for a dance, before he plunges into the ice?';
+
+        console.log("storkclub");
+
+    }
+
+
+
+    else if (newWords === sWords[6]) {
+
+        hint.innerHTML = "HINT: Who does the Mad Titan sacrifice to acquire the Soul Stone?";
+
+        console.log("gamora");
+
+    }
+
+
+
+    else if (newWords === sWords[7]) {
+
+        hint.innerHTML = 'HINT: What is the name of the little boy Tony befriends while stranded in the Iron Man 3?';
+
+        console.log("harley");
+
+    }
+
+
+
+    else if (newWords === sWords[8]) {
+
+        hint.innerHTML = 'HINT: What is the name of the organisation revealed to have taken over S.H.I.E.L.D. in Captain America: The Winter Soldier?';
+
+        console.log("hydra");
+
+    }
+
+
+
+
+
+    else if (newWords === sWords[9]) {
+
+        hint.innerHTML = "HINT: Director Taika Waititi also played which comedic Thor: Ragnarok character?";
+
+        console.log("cork");
+
+    }
+
+
+
+
+
+
+
+
+
+    else if (newWords === sWords[10]) {
+
+        hint.innerHTML = 'HINT:Who was the villain of ‘Thor: The Dark World?';
+
+        console.log("malekith");
+
+    }
+
+
+
+    else if (newWords === sWords[11]) {
+
+        hint.innerHTML = 'HINT:. Which planet does Peter Quill retrieve the Orb containing the Power Stone?';
+
+        console.log("morag");
+
+    }
+
+
 
 }
+
+
